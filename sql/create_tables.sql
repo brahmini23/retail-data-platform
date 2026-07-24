12796e5c461410c7fce8ed06863e10ea69ebf330
brahmini23 <brahmini.mandava@gmail.com>
2026-07-24 16:22:54 -0500

    Create tables sql and add python dependencies

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,41 +1,53 @@
 
 create table Customers (
-    customer_id INT PRIMARY KEY,
-    first_name VARCHAR,
-    last_name VARCHAR,
-    email VARCHAR,
-    city VARCHAR,
-    state VARCHAR,
-    signup_date DATE
+    customer_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    signup_date timestamptz NOT NULL
 )
 
 
 create table Categories (
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR
+    category_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL UNIQUE
 )
 
 
 create table Products (
-    product_id INT PRIMARY KEY,
-    category_id INT,
-    product_name VARCHAR,
-    price DECIMAL
+    product_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_id INT NOT NULL,
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+        REFERENCES Categories(category_id),
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
 )
 
 
 create table Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    status VARCHAR
+    order_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    customer_id INT NOT NULL,
+    CONSTRAINT fk_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES Customers(customer_id),
+    order_date timestamptz NOT NULL,
+    status VARCHAR(50) NOT NULL
 )
 
 
 create table Order_Items (
-    order_item_id INT PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    unit_price DECIMAL
+    order_item_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    order_id INT NOT NULL,
+    CONSTRAINT fk_order
+        FOREIGN KEY (order_id)
+        REFERENCES Orders(order_id),
+    product_id INT NOT NULL,
+    CONSTRAINT fk_product
+        FOREIGN KEY (product_id)
+        REFERENCES Products(product_id),
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL
 )
